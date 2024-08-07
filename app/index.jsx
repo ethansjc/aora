faeea140042cd5b4a539272de020b120f2f3b442
ethansjc <ethansjcheong@gmail.com>
2024-08-07 12:25:39 -0400

    Completed bottom tabs functionality

--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,11 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
+    <View className="w-full flex justify-center items-center h-full bg-white">
+      <Text className="text-3xl">Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to Profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Home
       </Link>
     </View>
   );
