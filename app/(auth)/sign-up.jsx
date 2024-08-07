920a8c7a29d24a8b534a6c219eca01728e9a906c
ethansjc <ethansjcheong@gmail.com>
2024-08-07 16:19:06 -0400

    incorporated appwrite with app

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,22 +1,36 @@
-import { View, Text, ScrollView, Image } from "react-native";
 import React, { useState } from "react";
+import { Link, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link } from "expo-router";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
-
 import CustomButton from "../../components/CustomButton";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
-    username:'',
+    username: "",
     email: "",
     password: "",
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please enter in all the fields");
+    }
+
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -34,7 +48,7 @@ const SignUp = () => {
           <FormField
             title="Username"
             value={form.username}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
             otherStyles="mt-10"
           />
           <FormField
@@ -51,7 +65,7 @@ const SignUp = () => {
             otherStyles="mt-7"
           />
           <CustomButton
-            title="Sign In"
+            title="Sign Up"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
